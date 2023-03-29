6a0fe6f9b2c620fe74a05b60913f6edf95e5c165
TayneB <tayne.bilyard@developersinstitute.co.nz>
2023-03-29 15:45:39 +1300

    Environmental Variable still broken

--- src/services/getProducts.js
+++ src/services/getProducts.js
@@ -2,11 +2,11 @@ const getProducts = async () => {
   // TODO Fetch products and prices from the Stripe API
   // Use the following URL for your fetch request
   const url = await fetch `${process.env.REACT_APP_API_URL}/products`;
-  
+  const burl = await url.json();
 
-  console.log(url);
+  console.log(burl);
 
-  const products = [];
+  const products = burl;
 
   return products;
 };
