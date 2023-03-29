4e13204874bc17ba6382416408c4feb3c2b790be
TayneB <tayne.bilyard@developersinstitute.co.nz>
2023-03-30 03:20:49 +1300

    Products displyaing correctly from component

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -20,7 +20,7 @@ const Product = ({
         Rarity: {rarity} | Type: {type}
       </h3>
       <p>{description}</p>
-      <button onClick={onClick}>Buy now {price} Gold Pieces or local equivalent</button>
+      <button onClick={onClick}>Buy now {price} Gold Pieces</button>
     </li>
   );
 };
