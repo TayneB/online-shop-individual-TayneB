976ee3414ce76a130ff428b795f62d940f0e1029
TayneB <tayne.bilyard@developersinstitute.co.nz>
2023-03-29 15:34:45 +1300

    Environmental Variables are innaccessible

--- src/services/getProducts.js
+++ src/services/getProducts.js
@@ -1,7 +1,10 @@
 const getProducts = async () => {
   // TODO Fetch products and prices from the Stripe API
   // Use the following URL for your fetch request
-  const url = `${process.env.REACT_APP_API_URL}/products`;
+  const url = await fetch `${process.env.REACT_APP_API_URL}/products`;
+  
+
+  console.log(url);
 
   const products = [];
 
